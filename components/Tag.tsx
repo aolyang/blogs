4a0ca6d07c065b2ff4307403b39493cb8530b876
aolyang <aolyang@foxmail.com>
2023-10-16 09:43:51 +0800

    style

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -18,7 +18,7 @@ const Tag = ({ text }: Props) => {
             <span className='inline-flex items-center gap-1 text-sm'>
                 {config.icon && <config.icon width='20' height='20' />}
                 <span style={{ color: config.color }}>
-                    <span className='py-1 hover:text-primary-600 dark:hover:text-primary-400'>
+                    <span className='inline-block py-1 hover:text-primary-600 dark:hover:text-primary-400'>
                         {content}
                     </span>
                 </span>
